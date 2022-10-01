186bd5e2aeaa6355fba7e219b23a0c0ec6266024
Alexey Guriev <guriev06@gmail.com>
2022-10-01 18:34:19 +0300

    Initial & stable

---
+++ app/channels/room_channel.rb
@@ -0,0 +1,21 @@
+class RoomChannel < ApplicationCable::Channel
+  def subscribed
+    logger.info "Subscribed to RoomChannel, roomId: #{params[:roomId]}"
+    @room = Room.find(params[:roomId])
+    stream_from "room_channel_#{@room.id}"
+    speak('message' => '* * * joined the room * * *')
+  end
+
+  def unsubscribed
+    logger.info "Unsubscribed from RoomChannel"
+    speak('message' => '* * * left the room * * *')
+  end
+
+  def speak(data)
+    logger.info "RoomChannel, speak: #{data.inspect}"
+
+    MessageService.new(
+      body: data['message'], user: current_user, room: @room
+    ).perform
+  end
+end
